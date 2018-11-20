5ab75f7a6e59e54638614b5480c27949d9fcf084
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-19 17:39:00 -0800

    Added starter date to ListOfCars.js

---
+++ doug-score/src/components/ListOfCars.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ListOfCars extends Component {
+    render() {
+        return (
+            <div>
+                Car List Will Go Here
+            </div>
+        );
+    }
+}
