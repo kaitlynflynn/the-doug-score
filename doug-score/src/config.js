bb59055b376622b6d8ab7324cefb7776bf7eaaed
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-20 18:22:18 -0800

    Added new file config.js to hold apiKey generated from Google APIs and imported spreadsheet ID

---
+++ doug-score/src/config.js
@@ -0,0 +1,6 @@
+export default {
+    apiKey: "AIzaSyCajRWjwdzgDva82aMT6BK5ZgxPZ1cS2ug", 
+    discoveryDocs: 
+      ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    spreadsheetId: "1KTArYwDWrn52fnc7B12KvjRb6nmcEaU6gXYehWfsZSo"
+  };
