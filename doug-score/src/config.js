a5a19c7c98e2579bf3dd038803f646856ff8af7e
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-20 18:22:53 -0800

    Added notes to config.js

--- doug-score/src/config.js
+++ doug-score/src/config.js
@@ -1,6 +1,6 @@
 export default {
-    apiKey: "AIzaSyCajRWjwdzgDva82aMT6BK5ZgxPZ1cS2ug", 
+    apiKey: "AIzaSyCajRWjwdzgDva82aMT6BK5ZgxPZ1cS2ug", // API key from Google APIs
     discoveryDocs: 
       ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-    spreadsheetId: "1KTArYwDWrn52fnc7B12KvjRb6nmcEaU6gXYehWfsZSo"
+    spreadsheetId: "1KTArYwDWrn52fnc7B12KvjRb6nmcEaU6gXYehWfsZSo" // ID from Google Spreadsheet
   };
