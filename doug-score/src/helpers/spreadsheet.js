bf2d3bd403c1755c4f252b0386c89e10b2d004c4
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-20 18:45:42 -0800

    Added export function to load callback; as well as some notes; imported config file

---
+++ doug-score/src/helpers/spreadsheet.js
@@ -0,0 +1,33 @@
+import config from "../config";
+
+// This code invokes our sheets API, retrieves the values from the spreadsheet by passing the spreadsheetID/range. Promise returns two responses; one if there is data and another if there is an error.
+
+/**
+ * Load the cars from the spreadsheet
+ * Get the right values from it and assign.
+ */
+export function load(callback) {
+  window.gapi.client.load("sheets", "v4", () => {
+    window.gapi.client.sheets.spreadsheets.values
+      .get({
+        spreadsheetId: config.spreadsheetId,
+        range: "Sheet1!A4:T"
+      })
+      .then(
+        response => {
+          const data = response.result.values;
+const cars = data.map(car => ({
+            year: car[0],
+            make: car[1],
+            model: car[2]
+          })) || [];
+callback({
+            cars
+          });
+        },
+        response => {
+          callback(false, response.result.error);
+        }
+      );
+  });
+}
