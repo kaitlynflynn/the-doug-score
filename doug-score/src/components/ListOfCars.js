82b33f241ffdaf9c594cf2250ac54643e2e23c38
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-20 18:41:57 -0800

    Added componentDidMount along with a couple other steps & imported config file

--- doug-score/src/components/ListOfCars.js
+++ doug-score/src/components/ListOfCars.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import config from '../config';
 
 class ListOfCars extends Component {
+
+componentDidMount() {
+    // Step 1. Load JavaScript client library
+    window.gapi.load("client", this.initClient);
+}
+
+initClient = () => {
+    // Step 2. Initialize JavaScript client library
+    window.gapi.client
+      .init({
+        apiKey: config.apiKey, // config is coming from the config.js file created
+        // API key automatically added to Discovery Document URLs
+        discoveryDocs: config.discoveryDocs
+      })
+      .then(() => {
+      // Step 3. Initialize/make the API request
+      load(this.onLoad);
+    });
+  };
+
     render() {
         return (
             <div>
