d84d79ad6d13d98766b604bfb7749445fabe7411
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-21 17:04:22 -0800

    Cleaned up code in App.js to remove unwanted code

--- doug-score/src/App.js
+++ doug-score/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import CarList from './components/CarList'; //importing in new component which will have our Cars List
 
@@ -8,19 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">The Doug Score - Stats</h1>
-          {/* <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
         </header>
         <CarList />
       </div>
