d6e502e8db165ad981ccab76fcac0a5b345a2b77
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-21 17:06:18 -0800

    Added comment on API Key

--- doug-score/src/config.js
+++ doug-score/src/config.js
@@ -1,5 +1,6 @@
 export default {
     apiKey: "AIzaSyCajRWjwdzgDva82aMT6BK5ZgxPZ1cS2ug", // API key from Google APIs
+    // Normally you would want to hide this key so others can't see it on git, but in this instance it is not sensitive data
     discoveryDocs: 
       ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
     spreadsheetId: "1KTArYwDWrn52fnc7B12KvjRb6nmcEaU6gXYehWfsZSo" // ID from Google Spreadsheet
